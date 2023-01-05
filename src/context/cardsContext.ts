7df7b09c5794bfed5f42fa5c6dc90adad095fdb0
lippiece <lippiece@proton.me>
2023-01-05 16:59:56 +0700

    feat: :truck: give each card its own source

--- src/context/cardsContext.ts
+++ src/context/cardsContext.ts
@@ -2,31 +2,31 @@ import { createContext } from "react";
 
 import CardType from "../@types/Card";
 
-const source = "https://picsum.photos/200s/300";
 export const defaultCards = {
   cards: [
     {
       id: 1,
-      image: source,
+      image: "https://picsum.photos/200/300",
     },
     {
       id: 2,
-      image: source,
+      image: "https://picsum.photos/300/300",
     },
     {
       id: 3,
-      image: source,
+      image: "https://picsum.photos/400/300",
     },
     {
       id: 4,
-      image: source,
+      image: "https://picsum.photos/400/400",
     },
     {
       id: 5,
-      image: source,
+      image: "https://picsum.photos/500/400",
     },
   ],
   score: 0,
+  toShow: 5,
 };
 export const CardsContext = createContext(defaultCards);
 export const CardsDispatchContext = createContext(defaultCards);
