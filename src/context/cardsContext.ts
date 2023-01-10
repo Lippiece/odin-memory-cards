6bff591f62a69c7ba74ecbd8e5cb52fbaad9e6a9
lippiece <lippiece@proton.me>
2023-01-10 08:34:18 +0700

    fix card ids
    Count from 1.

--- src/context/cardsContext.ts
+++ src/context/cardsContext.ts
@@ -1,13 +1,16 @@
 import { createContext } from "react";
+
 import getRandomSize from "../logic/getRandomSize";
-const _cards = Array.from({ length: 15 }, (value, index) => ({
-  id: index,
+
+const _cards = Array.from({ length: 15 }, (value, index) => 
+({
+  id   : index + 1,
   image: `https://picsum.photos/${getRandomSize(100, 600)}`,
 }));
 
 export const defaultCards = {
-  cards: _cards,
-  score: 0,
+  cards : _cards,
+  score : 0,
   toShow: _cards.slice(0, 5),
 };
 export const CardsContext = createContext(defaultCards);
